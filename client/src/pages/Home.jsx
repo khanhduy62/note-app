b5267b90918c7ea40dd913efff7b7f4a089010a0
Duy Le <lekhanhduyduy1201@gmail.com>
2024-11-19 11:35:42 +0700

    create UI for user menu

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import { Outlet, useLoaderData } from "react-router-dom";
+import UserMenu from "../components/UserMenu";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Typography variant="h4" sx={{ mb: "20px" }}>
+        Note App
+      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "right", mb: "10px" }}>
+        <UserMenu />
+      </Box>
+    </>
+  );
 }
